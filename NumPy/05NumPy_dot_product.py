a7861b71694bc0112fd379866f5c6824666e0c42
matheus costa <matheuscosta.tech@gmail.com>
2022-10-11 11:46:05 -0300

    Performing some calculations with numpy

---
+++ NumPy/05NumPy_dot_product.py
@@ -0,0 +1,11 @@
+import numpy as np
+import math
+
+u = (-1, 5)
+v = (2, -3)
+
+result = np.dot(u, v)
+
+print(result)
+
+math.cos
---
+++ NumPy/07NumPy_subtraction.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+u = np.array([-1, 5])
+v = np.array([2, -3])
+
+print(u - v)
