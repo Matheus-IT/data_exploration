6379ab90bc451d442d0d9fa932c0d430e172b220
matheus costa <matheuscosta.tech@gmail.com>
2022-10-31 22:45:22 -0300

    Balloons, almost there ...

--- NumPy/14baloes.py
+++ NumPy/14baloes.py
@@ -1,3 +1,23 @@
+from dataclasses import dataclass
+from typing import List, Union
+
+
+@dataclass
+class Dot:
+    x: int
+    y: int
+
+
+@dataclass
+class Segment:
+    start: Dot
+    end: Dot
+    segment_tilt_direction: Union[Dot, None]
+
+    def this_balloon_is_gonna_hit(self, balloon: Dot):
+        return self.start.x <= balloon.x <= self.end.x
+
+
 # Utils ---------------------------------------------------------------
 def read_numbers():
     try:
@@ -7,19 +27,58 @@ def read_numbers():
     return list(map(int, input_data.split(' ')))
 
 
+def segment_tilt_direction(x1, y1, x2, y2):
+    if not y1 != y2:
+        return None
+    if y1 > y2:
+        return Dot(x1, y1)
+    return Dot(x2, y2)
+
+
 # ---------------------------------------------------------------------
+
 
 while True:
     input_data = read_numbers()
     if not input_data:
         break
     line_segments, queries = input_data
-    print(line_segments, queries)
+
+    all_segments: List[Segment] = []
+    tilted_segments: List[Segment] = []
+    flat_segments: List[Segment] = []
 
     for i in range(line_segments):
         x1, y1, x2, y2 = read_numbers()
-        print(x1, y1, x2, y2)
+
+        segment = Segment(
+            start=Dot(x1, y1),
+            end=Dot(x2, y2),
+            segment_tilt_direction=segment_tilt_direction(x1, y1, x2, y2),
+        )
+
+        all_segments.append(segment)
+
+        if segment.segment_tilt_direction:
+            tilted_segments.append(segment)
+        else:
+            flat_segments.append(segment)
 
     for i in range(queries):
-        query = read_numbers()[0]
-        print(query)
+        balloon_x = read_numbers()[0]
+        balloon = Dot(x=balloon_x, y=0)
+
+        for segment in tilted_segments:
+            if segment.this_balloon_is_gonna_hit(balloon):
+                balloon.x = segment.segment_tilt_direction.x
+                balloon.y = segment.segment_tilt_direction.y
+
+        for segment in flat_segments:
+            if segment.this_balloon_is_gonna_hit(balloon):
+                balloon.y = segment.end.y
+                print(balloon.x, balloon.y)
+            else:
+                print(balloon.x)
+
+        if len(flat_segments) == 0:
+            print(balloon.x)
