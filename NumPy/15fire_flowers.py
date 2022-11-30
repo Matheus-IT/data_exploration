8c1d70c82a557932126c5a3dd931c043afc5aa08
matheus costa <matheuscosta.tech@gmail.com>
2022-11-29 22:03:34 -0300

    Assignment of in beecrowd

--- NumPy/15fire_flowers.py
+++ NumPy/15fire_flowers.py
@@ -1,8 +1,19 @@
 import sys
+from math import sqrt
 
 
 def read_numbers(input_data: str):
     return list(map(int, input_data.strip().split(' ')))
+
+
+def compute_distance_between_centers(x1, y1, x2, y2):
+    """Calculate the distance between the centers of the elipses"""
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def elipses_match(centers_distance, r1, r2):
+    """Tells if one elipse is inside the other"""
+    return centers_distance <= r1 - r2
 
 
 for line in sys.stdin:
@@ -11,4 +22,14 @@ for line in sys.stdin:
         break
 
     r1, x1, y1, r2, x2, y2 = read_numbers(input_data)
-    print(r1, x1, y1, r2, x2, y2)
+
+    centers_distance = compute_distance_between_centers(x1, y1, x2, y2)
+
+    match = elipses_match(centers_distance, r1, r2)
+
+    results = {
+        False: 'MORTO',
+        True: 'RICO',
+    }
+
+    print(results[match])
