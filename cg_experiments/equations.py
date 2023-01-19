dc9968376569e7366286e7d2692afdcfb5de509d
matheus costa <matheuscosta.tech@gmail.com>
2023-01-19 11:28:54 -0300

    Calculation of point of intersection

--- cg_experiments/equations.py
+++ cg_experiments/equations.py
@@ -5,3 +5,7 @@ def calc_ray_equation(p0, o, d, n=Matrix([[0], [1], [0], [0]])):
     numerator = Matrix(Matrix(p0) - Matrix(o)).dot(n)
     denominator = Matrix(d).dot(n)
     return numerator / denominator
+
+
+def calc_point_of_intersection(o, d, t):
+    return Matrix(o) + Matrix(d) * t
