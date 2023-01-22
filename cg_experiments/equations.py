4d5da47ada5eb94e541726b79e7f3e08c86e453f
matheus costa <matheuscosta.tech@gmail.com>
2023-01-22 15:07:05 -0300

    Small tweak on equation return

--- cg_experiments/equations.py
+++ cg_experiments/equations.py
@@ -1,11 +1,12 @@
-from sympy import Matrix
+from sympy import Matrix, simplify
 
 
-def calc_ray_equation(p0, o, d, n=Matrix([[0], [1], [0], [0]])):
+def calc_ray_intersection_with_surface(p0, o, d, n=Matrix([[0], [1], [0], [0]])):
     numerator = Matrix(Matrix(p0) - Matrix(o)).dot(n)
     denominator = Matrix(d).dot(n)
-    return numerator / denominator
+    return simplify(f"{numerator}/{denominator}")
 
 
 def calc_point_of_intersection(o, d, t):
-    return Matrix(o) + Matrix(d) * t
+    p = Matrix(o) + Matrix(d) * t
+    return p[:3]
