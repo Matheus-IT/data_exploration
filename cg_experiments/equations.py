456e5078edb9c81fb40cd37583e2985e449e6f8a
matheus costa <matheuscosta.tech@gmail.com>
2023-01-25 11:36:48 -0300

    Calculation for the intersection with cylinder

--- cg_experiments/equations.py
+++ cg_experiments/equations.py
@@ -22,3 +22,18 @@ def calc_ray_intersection_with_sphere(d, o):
     x = Symbol("x")
 
     return solve(a * x**2 + b * x + c)
+
+
+def calc_ray_intersection_with_cylinder(d, o):
+    from sympy.abc import x
+
+    d, o = Matrix(d), Matrix(o)
+
+    dx, dz = d[0], d[2]
+    ox, oz = o[0], o[2]
+
+    a = dx**2 + dz**2
+    b = 2 * ox * dx + 2 * oz * dz
+    c = ox**2 + oz**2 - 1
+
+    return solve(a * x**2 + b * x + c, x)
