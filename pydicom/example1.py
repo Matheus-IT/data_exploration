7aee7d9e3eaaa0ab3d317ba764781e72f02df54d
matheus costa <matheuscosta.tech@gmail.com>
2023-02-12 00:04:57 -0300

    Using test data

--- pydicom/example1.py
+++ pydicom/example1.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pydicom
+from pydicom.data import get_testdata_file
 from PIL import Image
 
 
-ds = pydicom.read_file("pydicom/CT_small.dcm")
-image = ds.pixel_array.astype(float)
-rescaled = (np.maximum(image, 0) / image.max()) * 255
-final_image = np.uint8(rescaled)
-Image.fromarray(final_image).show()
+with pydicom.dcmread(get_testdata_file("CT_small.dcm")) as ds:
+    print(ds)
+    image = ds.pixel_array.astype(float)
+    rescaled = (np.maximum(image, 0) / image.max()) * 255
+    final_image = np.uint8(rescaled)
+    Image.fromarray(final_image).show()
