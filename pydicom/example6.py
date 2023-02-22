742ce62f2f3e0299247c84630a04885021471b8c
matheus costa <matheuscosta.tech@gmail.com>
2023-02-22 10:20:29 -0300

    Removing extra step when opening image

--- pydicom/example6.py
+++ pydicom/example6.py
@@ -11,6 +11,5 @@ matplotlib.use("TkAgg")
 
 with pydicom.dcmread("pydicom/image5.dcm") as ds:
     img = ds.pixel_array.astype(np.int16)
-    img = np.array(img, dtype=np.int16)
     plt.imshow(img, cmap=plt.cm.bone)
     plt.show()
