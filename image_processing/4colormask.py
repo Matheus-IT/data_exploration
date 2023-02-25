3c6f47519b5935aeeb915b04941ac8cbcbeeefc3
matheus costa <matheuscosta.tech@gmail.com>
2023-02-25 20:14:11 -0300

    Trying color masks

--- image_processing/4colormask.py
+++ image_processing/4colormask.py
@@ -1,8 +1,7 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 import pydicom
-
 
 # Carregar imagem
 ds = pydicom.dcmread("image_processing/images/mammography.dcm")
@@ -13,10 +12,13 @@ pixel_data = ds.pixel_array
 # Convert pixel data to 8-bit integer
 img = cv2.normalize(pixel_data, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
 
-_, thresh = cv2.threshold(img, np.mean(img), 255, cv2.THRESH_BINARY_INV)
+low = 10
+high = 100
 
-edges = cv2.dilate(cv2.Canny(thresh, 0, 255), None)
+mask = cv2.inRange(img, low, high)
+
+result = cv2.bitwise_and(img, img, mask=mask)
 
 plt.axis("off")
-plt.imshow(edges)
+plt.imshow(result)
 plt.show()
