c673e16107aaccb577fa03b3400cbe4dbcf1f10c
matheus costa <matheuscosta.tech@gmail.com>
2023-02-25 18:13:16 -0300

    Renaming stuff

--- image_processing/1thresholding_otsu.py
+++ image_processing/1thresholding_otsu.py
@@ -11,7 +11,7 @@ import pydicom
 
 
 # Carregar imagem
-ds = pydicom.dcmread("image_processing/images/image.dcm")
+ds = pydicom.dcmread("image_processing/images/mammography.dcm")
 
 # Get pixel data and metadata
 pixel_data = ds.pixel_array
