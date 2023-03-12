0c02d2b43a805b54ae8b7f729e4e84be1de75079
matheus costa <matheuscosta.tech@gmail.com>
2023-03-12 16:13:39 -0300

    Showing dicom image with matplotlib

--- image_processing/show_image.py
+++ image_processing/show_image.py
@@ -1,15 +1,17 @@
 import pydicom
+from pydicom import FileDataset
 import cv2
 import matplotlib.pyplot as plt
 
-# Carregar imagem
-ds = pydicom.dcmread("image_processing/images/mammography.dcm")
+
+img_path = "image_processing/images/mammography.dcm"
+ds = pydicom.dcmread(img_path)
 
 # Get pixel data and metadata
-pixel_data = ds.pixel_array
+img = ds.pixel_array
 
 # Convert pixel data to 8-bit integer
-img = cv2.normalize(pixel_data, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+# img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
 
 plt.imshow(img, cmap="bone")
 plt.show()
