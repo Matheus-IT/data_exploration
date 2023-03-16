3c04df1df3fbb0d43dce78f51bc49c73ed955288
matheus costa <matheuscosta.tech@gmail.com>
2023-03-15 23:57:51 -0300

    Drawing on dicom image and then saving it

--- image_processing/9changing_dicom_image.py
+++ image_processing/9changing_dicom_image.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pydicom
-from PIL import Image, ImageDraw
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+import cv2 as cv
+from utils.presentation import show_pixel_info, show_pixel_array_info
 
 
 # Read the original dicom file
@@ -12,18 +14,8 @@ ds_copy = ds.copy()
 # Get the pixel array of the image
 img = ds_copy.pixel_array
 
-# Convert the pixel array to a PIL image
-img = Image.fromarray(img)
-
-# Draw a square on the image using PIL
-draw = ImageDraw.Draw(img)
-draw.rectangle(
-    [100, 100, 200, 200],  # Top-left and bottom-right coordinates of the square
-    outline="red",
-)
-
-# Convert the PIL image back to a pixel array
-img = np.array(img)
+# Draw a square on the image
+cv.rectangle(img, (500, 500), (1000, 1000), color=(4096, 4096, 4096), thickness=20)
 
 # Update the pixel array of the copy with the modified image
 ds_copy.PixelData = img.tobytes()
@@ -32,7 +24,7 @@ ds_copy.Rows, ds_copy.Columns = img.shape
 # Increment the instance number by one for the copy
 ds_copy.InstanceNumber += 1
 # Generate a new SOP Instance UID for the second file
-ds_copy.SOPInstanceUID = pydicom.uid.generate_uid()
+ds_copy.SOPInstanceUID = pydicom.uid.generate_uid()  # type: ignore
 
 # Save the modified dicom file as a new file name
 ds_copy.save_as("image_processing/images/mammography_modified.dcm")
--- image_processing/show_image.py
+++ image_processing/show_image.py
@@ -1,17 +1,19 @@
 import pydicom
-from pydicom import FileDataset
-import cv2
 import matplotlib.pyplot as plt
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+from utils.presentation import (
+    show_pixel_info,
+    show_voi_lut_module,
+    show_file_dataset_info,
+    show_pixel_array_info,
+)
 
 
-img_path = "image_processing/images/mammography.dcm"
+img_path = "image_processing/images/mammography_modified.dcm"
 ds = pydicom.dcmread(img_path)
 
 # Get pixel data and metadata
 img = ds.pixel_array
 
-# Convert pixel data to 8-bit integer
-# img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-
-plt.imshow(img, cmap="bone")
+plt.imshow(img, cmap="gray")
 plt.show()
