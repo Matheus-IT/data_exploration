7273e649c54336a65980997a14abb994a3897d72
matheus costa <matheuscosta.tech@gmail.com>
2023-04-01 20:30:29 -0300

    Able to separate cluster with higher intensity

--- image_processing/2kmeans.py
+++ image_processing/2kmeans.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+import cv2 as cv
 import pydicom
+from utils.image_normalization import normalize, denormalize
 
 
 # Carregar imagem
@@ -11,32 +12,34 @@ ds = pydicom.dcmread("image_processing/images/mammography.dcm")
 pixel_data = ds.pixel_array
 
 # Convert pixel data to 8-bit integer
-img = cv2.normalize(pixel_data, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+img = normalize(pixel_data)
 
 # Resize image, or else it will be displayed too large
-img = cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
-
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv.resize(img, (0, 0), fx=0.1, fy=0.1)
 
 # Reshaping the image into a 2D array of pixels and 3 color values (RGB)
-pixel_vals = img.reshape((-1, 3))
+pixel_vals = img.reshape((-1, 1))
 
 # Convert to float type only for supporting cv2.kmean
 pixel_vals = np.float32(pixel_vals)
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
-k = 3  # Choosing number of cluster
-retval, labels, centers = cv2.kmeans(
-    pixel_vals, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+k = 5  # Choosing number of cluster
+retval, labels, centers = cv.kmeans(
+    pixel_vals, k, None, criteria, 10, cv.KMEANS_PP_CENTERS
 )
 
-centers = np.uint8(centers)
+centers = np.array(centers, dtype=np.uint8)
 
-# Mapping labels to center points( RGB Value)
+labels[(labels != centers.argmax())] = 0
+
+# Mapping labels to center points
 segmented_data = centers[labels.flatten()]
 
 # reshape data into the original image dimensions
 segmented_image = segmented_data.reshape((img.shape))
 
-plt.imshow(segmented_image)
-plt.show()
+images = cv.hconcat([img, segmented_image])
+cv.imshow("images", images)
+cv.waitKey(0)
+cv.destroyAllWindows()
