41400bf620ed8d8ed2dae582a9c40eb919d64c4e
matheus costa <matheuscosta.tech@gmail.com>
2023-04-22 19:25:18 -0300

    simple function to compare filtered image

--- image_processing/image_processing_course/utils.py
+++ image_processing/image_processing_course/utils.py
@@ -2,16 +2,22 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
 
 
-def compare_image_filter(img: Image, operation: callable):
+def compare_image_filter(img: Image, operation: callable, gray=False):
     fig = plt.figure(figsize=(8, 5))
     fig.add_subplot(1, 2, 1)
 
-    plt.imshow(img)
+    if gray:
+        plt.imshow(img, cmap="gray")
+    else:
+        plt.imshow(img)
     plt.title("Original")
 
     fig.add_subplot(1, 2, 2)
 
     filtered = operation(img)
-    plt.imshow(filtered)
+    if gray:
+        plt.imshow(filtered, cmap="gray")
+    else:
+        plt.imshow(filtered)
     plt.title("Filtered")
     plt.show()
