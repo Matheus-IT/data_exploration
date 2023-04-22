2167b8f0a3956ab7e3f75eaf5ed8d8b7ef204d59
matheus costa <matheuscosta.tech@gmail.com>
2023-04-21 22:05:08 -0300

    Exploring some new ideas with kmeans

---
+++ image_processing/exploring.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import pydicom
+from utils.image_normalization import normalize, denormalize
+
+
+image = pydicom.dcmread(
+    "image_processing/images/dicom/ex1/DICOM/09266278/7CF888A1/73F2A227.dcm"
+).pixel_array
+
+# Convert pixel data to 8-bit integer
+img = normalize(image)
+
+# Invert the image
+img = 255 - img
+
+# Resize image, or else it will be displayed too large
+img = cv.resize(img, (0, 0), fx=0.1, fy=0.1)
+
+# Reshaping the image into a 2D array of pixels and 3 color values (RGB)
+pixel_vals = img.reshape((-1, 1))
+
+# Convert to float type only for supporting cv2.kmean
+pixel_vals = np.float32(pixel_vals)
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+k = 5  # Choosing number of cluster
+retval, labels, centers = cv.kmeans(
+    pixel_vals, k, None, criteria, 10, cv.KMEANS_PP_CENTERS
+)
+
+centers = np.array(centers, dtype=np.uint8)
+
+# labels[(labels != centers.argmax())] = 0
+
+# Mapping labels to center points
+segmented_data = centers[labels.flatten()]
+
+# reshape data into the original image dimensions
+segmented_image = segmented_data.reshape((img.shape))
+
+images = cv.hconcat([img, segmented_image])
+cv.imshow("images", images)
+cv.waitKey(0)
+cv.destroyAllWindows()
