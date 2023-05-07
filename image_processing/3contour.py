4192b5fd6033361108c49bfdcffa1d99d56c28db
matheus costa <matheuscosta.tech@gmail.com>
2023-05-06 22:55:13 -0300

    Fixing old script

--- image_processing/3contour.py
+++ image_processing/3contour.py
@@ -5,7 +5,7 @@ import pydicom
 
 
 # Carregar imagem
-ds = pydicom.dcmread("image_processing/images/mammography.dcm")
+ds = pydicom.dcmread("image_processing/images/dicom/mammography.dcm")
 
 # Get pixel data and metadata
 pixel_data = ds.pixel_array
