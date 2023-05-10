57568752dd1a435c40dcd0d22de3c4e9caf81eae
matheus costa <matheuscosta.tech@gmail.com>
2023-05-09 21:52:43 -0300

    Trying to remove the background and apply kmeans

--- image_processing/exploring4.py
+++ image_processing/exploring4.py
@@ -12,41 +12,45 @@ from utils.image_normalization import normalize, denormalize
 from icecream import ic
 import numpy as np
 from utils.elapsed_time import Timer
+from PIL import Image
 
 
 with Timer():
     # reading image
-    original = cv.imread("image_processing/images/mammography2.png")
-    original = cv.cvtColor(original, cv.COLOR_BGRA2GRAY)
-    original = cv.flip(original, 1)
-    modified = original.copy()
+    original = pydicom.dcmread(
+        "image_processing/images/dicom/mammography2.dcm"
+    ).pixel_array
 
-    x1 = int(0.9 * original.shape[0])
-    y1 = int(0.99 * original.shape[1])
+    modified = normalize(original)
 
-    x2 = int(0.9 * original.shape[0])
-    y2 = int(0.1 * original.shape[1])
+    h, w = modified.shape
+    modified = modified.copy()
 
-    thresh = (original[x1, y1] + original[x2, y2]) // 2
+    kernel_size = (5, 5)
+    modified = cv.GaussianBlur(modified, kernel_size, 0)
 
     # Convert the new image to binary
-    ret, modified = cv.threshold(original, thresh, 255, cv.THRESH_BINARY)
+    ret, modified = cv.threshold(modified, 0, 255, cv.THRESH_OTSU)
 
-    # erosion
-    modified = cv.erode(modified, None, iterations=5)
-    # dilation
-    modified = cv.dilate(modified, None, iterations=5)
+    # opening morphological operation
+    modified = cv.erode(modified, None, iterations=10)
+    modified = cv.dilate(modified, None, iterations=10)
 
-    modified = cv.bitwise_and(original, original, mask=modified)
+    # dilate mask to fill in the gaps
+    modified = cv.dilate(modified, None, iterations=10)
 
-    plt.imshow(modified, cmap="gray")
-    plt.show()
+    # get largest contour
+    contours = cv.findContours(modified, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    big_contour = max(contours, key=cv.contourArea)
 
-    exit()
+    # draw largest contour as white filled on black background as mask
+    mask = np.zeros((h, w), dtype=np.uint8)
+    cv.drawContours(mask, [big_contour], 0, 255, cv.FILLED)
 
-    # smoothing
-    modified = original.copy()
-    kernel_size = (5, 5)
+    modified = cv.bitwise_and(original, original, mask=mask)
+
+    kernel_size = (7, 7)
     modified = cv.GaussianBlur(modified, kernel_size, 0)
 
     # Reshaping the image into a 2D array of pixels and 3 color values (RGB)
@@ -54,12 +58,16 @@ with Timer():
     pixel_vals = modified.flatten().astype(np.float32)
 
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    k = 3  # Choosing number of clusters
+    k = 5  # Choosing number of clusters
     retval, labels, centers = cv.kmeans(
         pixel_vals, k, None, criteria, 10, cv.KMEANS_PP_CENTERS
     )
 
-    # centers = np.array(centers, dtype=np.uint8)
+    centers = np.array(centers, dtype=np.uint8)
+
+    ic(np.unique(labels))
+    ic(centers)
+    ic(retval)
 
     # Find the cluster with the highest intensity value
     centers_max = np.max(centers, axis=1)
@@ -80,10 +88,8 @@ with Timer():
     # Convert the new image to binary
     ret, modified = cv.threshold(img_high_intensity, 0, 255, cv.THRESH_BINARY)
 
-    # erosion
-    modified = cv.erode(modified, None, iterations=12)
-    # dilation
-    modified = cv.dilate(modified, None, iterations=12)
+    modified = cv.erode(modified, None, iterations=5)
+    modified = cv.dilate(modified, None, iterations=5)
 
     modified = modified.astype(np.uint8)
 
@@ -102,6 +108,8 @@ with Timer():
     # Define the coordinates of the red square
     top_left = max_loc
     bottom_right = (top_left[0] + subimg.shape[1], top_left[1] + subimg.shape[0])
+
+    original = cv.cvtColor(original, cv.COLOR_GRAY2BGR)
 
     # Draw the red square on the image
     cv.rectangle(original, top_left, bottom_right, (0, 0, 255), 2)
