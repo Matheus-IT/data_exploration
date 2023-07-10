c41f2c7d1ca3a33f7616ee01c6671c566ad6476a
matheuscosta <matheuscosta.tech@gmail.com>
2023-07-10 20:27:59 -0300

    Building chart to show documents per year

---
+++ seaborn/documents_per_year.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+data = {
+    "Year": [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+    "Documents": [18, 16, 24, 21, 10, 13, 22, 26, 25, 15, 2],
+}
+
+df = pd.DataFrame(data)
+
+sns.lineplot(x="Year", y="Documents", data=df)
+
+plt.xlabel("Ano")
+plt.ylabel("Documentos")
+plt.xticks(data["Year"])
+plt.show()
