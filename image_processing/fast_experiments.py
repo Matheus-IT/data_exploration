cfe5405b17855c352658900671c0462bf21cbb52
matheuscosta <matheuscosta.tech@gmail.com>
2023-07-24 20:28:22 -0300

    Trying out high pass filter

--- image_processing/fast_experiments.py
+++ image_processing/fast_experiments.py
@@ -16,8 +16,34 @@ import pydicom
 
 
 with Timer():
-    MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/nbia/CMMD/D1-0001/07-18-2010-NA-NA-79377/1.000000-NA-70244/"
-    ds = pydicom.dcmread(MAMMOGRAPHY_DATASET_PATH + "1-1.dcm")
-    original = ds.pixel_array
-    # Image.fromarray(original).show()
-    ic(ds)
+    MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/nbia/CMMD/D2-0001/07-18-2011-NA-NA-75485/1.000000-NA-12786/"
+    original = pydicom.dcmread(MAMMOGRAPHY_DATASET_PATH + "1-1.dcm").pixel_array
+
+    modified = original.copy()
+
+    modified = normalize(modified)
+
+    height, width = modified.shape
+
+    kernel_size = (5, 5)
+    modified = cv.GaussianBlur(modified, kernel_size, 0)
+
+    # Convert the new image to binary
+    ret, modified = cv.threshold(modified, 0, 255, cv.THRESH_OTSU)
+
+    # get largest contour
+    contours, hierarchy = cv.findContours(
+        modified, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
+    )
+    big_contour = max(contours, key=cv.contourArea)
+
+    # draw largest contour as white filled on black background as mask
+    mask = np.zeros((height, width), dtype=np.uint8)
+    cv.drawContours(mask, [big_contour], 0, 255, cv.FILLED)
+
+    modified = cv.bitwise_and(original, original, mask=mask)
+
+    modified = cv.resize(modified, (680, 520), interpolation=cv.INTER_CUBIC)
+    modified = modified - cv.GaussianBlur(modified, (21, 21), 3) + 127
+
+    Image.fromarray(modified).show()
