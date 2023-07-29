36676c9413978bc9e1696dc19069df97a3e38242
matheuscosta <matheuscosta.tech@gmail.com>
2023-07-29 13:05:26 -0300

    Refactoring detection code

--- tcc_project/detection.py
+++ tcc_project/detection.py
@@ -7,75 +7,37 @@ from utils.image_normalization import normalize
 import numpy as np
 from utils.elapsed_time import Timer
 from PIL import Image
+from steps import (
+    segment_breast_tissue,
+    paint_fragments_in_red,
+    apply_global_threshold,
+    get_roi_from_mask,
+    detect_contours_of_artifacts,
+    mark_roi_in_original_image,
+)
 
 
 with Timer():
-    MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/mysubdataset/subdataset_v3/D1-1087/1-2.dcm"
+    MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/my_subdataset/subdataset_v4/D3-0001/1-1.dcm"
     ds = pydicom.dcmread(MAMMOGRAPHY_DATASET_PATH)
     original = ds.pixel_array
 
-    # Normalizing image
-    original = normalize(original)
+    original = cv.normalize(original, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
 
     modified = original.copy()
 
-    # Segmenting breast tissue
-    kernel_size = (5, 5)
-    modified = cv.GaussianBlur(modified, kernel_size, 0)
+    modified = segment_breast_tissue(modified, original)
 
-    # Convert the new image to binary
-    ret, modified = cv.threshold(modified, 0, 255, cv.THRESH_OTSU)
-
-    # get largest contour
-    contours, hierarchy = cv.findContours(
-        modified, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
-    )
-    big_contour = max(contours, key=cv.contourArea)
-
-    # draw largest contour as white filled on black background as mask
-    height, width = modified.shape
-    mask = np.zeros((height, width), dtype=np.uint8)
-    cv.drawContours(mask, [big_contour], 0, 255, cv.FILLED)
-
-    modified = cv.bitwise_and(original, original, mask=mask)
-
-    modified = cv.equalizeHist(modified)
-
-    # Apply high pass filter
     modified = high_pass_filter(modified)
 
-    # get the high intensity cluster using kmeans
-    # ret, modified = cv.threshold(modified, 150, 255, cv.THRESH_BINARY)
+    modified = apply_global_threshold(modified)
+
+    roi = get_roi_from_mask(original, modified)
+
+    roi = detect_contours_of_artifacts(roi)
+
+    roi = paint_fragments_in_red(roi)
+
+    modified = mark_roi_in_original_image(original, roi)
+
     display_side_by_side(original, modified)
-    # Convert the new image to binary
-    # ret, modified = cv.threshold(modified, 0, 255, cv.THRESH_BINARY)
-
-    modified = modified.astype(np.uint8)
-
-    roi = cv.bitwise_and(original, original, mask=modified)
-
-    # Perform a morphological opening filter
-    roi = opening_filter(roi, iter=5, kernel_size=2)
-
-    roi = normalize(roi)
-
-    # Detect contours of artifacts
-    roi = cv.Canny(roi, 100, 200)
-    # increase thickness
-    roi = cv.dilate(roi, None, iterations=1)
-
-    # Painting roi fragments in red
-    roi = cv.cvtColor(roi, cv.COLOR_GRAY2BGR)
-    # Set the red channel values to 255
-    blue_channel = roi[:, :, 0]  # Extract the blue channel (channel index 0)
-    green_channel = roi[:, :, 1]  # Extract the green channel (channel index 1)
-    roi[0] = 255  # set red channel to maximum
-    roi[green_channel > 0, 1] = 0  # Set green channel values to 0
-    roi[blue_channel > 0, 2] = 0  # Set blue channel values to 0
-
-    original = cv.cvtColor(original, cv.COLOR_GRAY2BGR)
-
-    # Mark roi in original image
-    modified = cv.bitwise_or(original, roi)
-
-    # display_side_by_side(original, modified)
---
+++ tcc_project/steps.py
@@ -0,0 +1,71 @@
+import cv2 as cv
+import numpy as np
+from utils.filters import opening_filter
+
+
+def segment_breast_tissue(image, original_image):
+    kernel_size = (5, 5)
+    image = cv.GaussianBlur(image, kernel_size, 0)
+
+    # Convert the new image to binary
+    ret, image = cv.threshold(image, 0, 255, cv.THRESH_OTSU)
+
+    # get largest contour
+    contours, hierarchy = cv.findContours(
+        image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
+    )
+    big_contour = max(contours, key=cv.contourArea)
+
+    # draw largest contour as white filled on black background as mask
+    height, width = image.shape
+    mask = np.zeros((height, width), dtype=np.uint8)
+    cv.drawContours(mask, [big_contour], 0, 255, cv.FILLED)
+
+    image = cv.bitwise_and(original_image, original_image, mask=mask)
+
+    return image
+
+
+def enhance_contrast(image):
+    return cv.equalizeHist(image)
+
+
+def apply_global_threshold(image):
+    ret, image = cv.threshold(image, 170, 255, cv.THRESH_BINARY)
+    return image
+
+
+def get_roi_from_mask(image, mask):
+    roi = cv.bitwise_and(image, image, mask=mask)
+
+    # Perform a morphological opening filter
+    roi = opening_filter(roi, iter=5, kernel_size=2)
+
+    roi = cv.normalize(roi, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+    return roi
+
+
+def detect_contours_of_artifacts(roi):
+    # detect contours
+    roi = cv.Canny(roi, 100, 200)
+    # increase thickness
+    return cv.dilate(roi, None, iterations=1)
+
+
+def paint_fragments_in_red(img):
+    roi = img.copy()
+    roi = cv.cvtColor(roi, cv.COLOR_GRAY2BGR)
+    # Set the red channel values to 255
+    blue_channel = roi[:, :, 0]  # Extract the blue channel (channel index 0)
+    green_channel = roi[:, :, 1]  # Extract the green channel (channel index 1)
+    roi[0] = 255  # set red channel to maximum
+    roi[green_channel > 0, 1] = 0  # Set green channel values to 0
+    roi[blue_channel > 0, 2] = 0  # Set blue channel values to 0
+    return roi
+
+
+def mark_roi_in_original_image(original, roi):
+    # Mark roi in original image
+    original = cv.cvtColor(original, cv.COLOR_GRAY2BGR)
+    modified = cv.bitwise_or(original, roi)
+    return modified
