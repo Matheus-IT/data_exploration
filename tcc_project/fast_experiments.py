a18bd0c18efe027ed9ba86ce4ccbd0c0ffabe1f7
matheuscosta <matheuscosta.tech@gmail.com>
2023-08-04 22:55:08 -0300

    Trying to contribute

--- tcc_project/fast_experiments.py
+++ tcc_project/fast_experiments.py
@@ -20,5 +20,5 @@ import pydicom
 with Timer():
     # MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/nbia/CMMD/D2-0001/07-18-2011-NA-NA-75485/1.000000-NA-12786/"
     # original = pydicom.dcmread(MAMMOGRAPHY_DATASET_PATH + "1-1.dcm").pixel_array
-    original = cv.imread("image_processing/images/mammography2.png")
+    original = cv.imread("image_processing/images/mammography.png")
     Image.fromarray(original).show()
