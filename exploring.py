40ba16ef6c15d89df3e4bffb43c8d359bdfd4096
matheuscosta <matheuscosta.tech@gmail.com>
2023-11-08 21:07:56 -0300

    Playing around with regex in python

--- exploring.py
+++ exploring.py
@@ -1,1 +1,30 @@
-print('trying to contribute today, this will change')
+import re
+
+
+my_string = """FETO: Único ( X )	Gemelar (	)
+SITUAÇÃO: Longitudinal ( X )   	Transverso (   )
+APRESENTAÇÃO: Cefálico ( X )   	Pélvico (   )         (  ) Variável
+BATIMENTOS CARDÍACOS FETAIS:
+Presente ( X )   FC: [[Fetal Heart Rate]]	Ausente ( 	)
+MOVIMENTOS FETAIS: Presentes ( X ) Ausentes (  )
+PLACENTA:
+Anterior ( X )  Posterior (   )  Fúndica (   )  Prévia (	)
+Grau __ Colo uterino ________
+_______________
+CORDÃO UMBILICAL: _________
+BIOMETRIA FETAL:
+DBP: [[Biparietal Diameter]]		CF: [[Femur Length]]
+CA: [[Abdominal Circumference]]	CC: [[Head Circumference]]
+CNN: ___ cm         		PESO: [[Estimated Weight]]
+OFD: [[Occipital-Frontal Diameter]]	BASED GA:_
+BASED US:_
+LÍQUIDO:
+Oligodrâmnio (	)   Polidrâmnio (	)   Normal (   )
+ILA: [[Amniotic Fluid Index]]
+
+CONCLUSÃO: 
+
+- BIOMETRIA FETAL COMPATIVEL COM SEMANAS E DIAS 
+- ADEQUADO PARA IDADE GESTACIONAL [[Length]]"""
+
+print(re.findall(r'\[\[.+?\]\]', my_string))
