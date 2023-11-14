149a6d0384a2c8b0f8232cc69070e974bca63bdf
Matheus-IT <matheuscosta.tech@gmail.com>
2023-11-13 21:01:26 -0300

    Trying to show processed image

--- image_processing/19predominant_colors.py
+++ image_processing/19predominant_colors.py
@@ -1,9 +1,42 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import cv2 as cv
+import numpy as np
 
 
 LABEL_IMAGE_SIZE = (200, 200)
+
+
+# helpers ---------------------------------------------------------------------
+def centroidHistogram(clt):
+    # Create a histrogram for the clusters based on the pixels in each cluster
+    # Get the labels for each cluster
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+
+    # Create our histogram 
+    (hist, _) = np.histogram(clt.labels_, bins = numLabels)
+
+    # normalize the histogram, so that it sums to one
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+
+def plotColors(hist, centroids):
+    # Create our blank barchart
+    bar = np.zeros((100, 500, 3), dtype = "uint8")
+
+    x_start = 0
+    # iterate over the percentage and dominant color of each cluster
+    for (percent, color) in zip(hist, centroids):
+      # plot the relative percentage of each cluster
+      end = x_start + (percent * 500)
+      cv.rectangle(bar, (int(x_start), 0), (int(end), 100),
+        color.astype("uint8").tolist(), -1)
+      x_start = end
+    return bar
+# -----------------------------------------------------------------------------
 
 
 class ImageProcessorApp(tk.Tk):
@@ -25,21 +58,43 @@ class ImageProcessorApp(tk.Tk):
         upload_button = tk.Button(self, text="Upload Image", command=self.upload_image)
         upload_button.pack(pady=100)
 
+        process_button = tk.Button(self, text='Process colors', command=self.process_image_colors)
+        process_button.place(relx=0.5, rely=0.8, anchor="center")
+
     def upload_image(self):
         # Open a file dialog to select an image file
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
 
         if file_path:
-            # Open and process the image using PIL
-            print(f'Image path {file_path}')
-            image = Image.open(file_path)
+            self.image_to_process = file_path
 
             # Display the image in the Tkinter window
-            photo = ImageTk.PhotoImage(image.resize(LABEL_IMAGE_SIZE))
+            photo = ImageTk.PhotoImage(Image.open(file_path).resize(LABEL_IMAGE_SIZE))
             self.image_label.config(image=photo)
             self.image_label.image = photo  # to prevent garbage collection
 
-            # Perform your image processing operations here
+    def process_image_colors(self):
+        print('process_image_colors')
+        from sklearn.cluster import KMeans
+
+        self.image_to_process = cv.imread(self.image_to_process)
+        # We reshape our image into a list of RGB pixels
+        self.image_to_process = cv.cvtColor(self.image_to_process, cv.COLOR_BGR2RGB)
+        self.image_to_process = self.image_to_process.reshape((self.image_to_process.shape[0] * self.image_to_process.shape[1], 3))
+
+        number_of_clusters = 5
+        clt = KMeans(number_of_clusters)
+        clt.fit(self.image_to_process)
+
+        hist = centroidHistogram(clt)
+        bar = plotColors(hist, clt.cluster_centers_)
+        bar = Image.fromarray(bar)
+        bar.show()
+        bar = ImageTk.PhotoImage(bar.resize(LABEL_IMAGE_SIZE))
+        # Create a label to display the image on top of the button
+        bar_image_label = tk.Label(self, image=bar)
+        bar_image_label.place(relx=0.5, rely=1.0, anchor="center")
+
 
 if __name__ == "__main__":
     app = ImageProcessorApp()
