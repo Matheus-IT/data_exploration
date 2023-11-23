9978a5668a0e1b4cd7351c8d2188d7db3ec867be
matheuscosta <matheuscosta.tech@gmail.com>
2023-11-23 19:57:23 -0300

    Extract text from image

--- image_processing/20extract_text_from_image.py
+++ image_processing/20extract_text_from_image.py
@@ -1,5 +1,17 @@
+import pytesseract
+from PIL import Image
+import numpy as np
+
+
 def main():
-    pass
+    img = Image.open('image_processing/images/skyrim_image.jpg')
+    img.show()
+    img = np.array(img)
+    img[img < 150] = 0
+    Image.fromarray(img).show()
+
+    output = pytesseract.image_to_string(img)
+    print(output)
 
 
 if __name__ == '__main__':
