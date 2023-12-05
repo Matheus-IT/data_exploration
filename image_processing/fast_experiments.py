7af09c77153ec12f4aae1d63b4000573176d1c78
matheuscosta <matheuscosta.tech@gmail.com>
2023-12-04 22:51:32 -0300

    Playing around with DICOM

--- image_processing/fast_experiments.py
+++ image_processing/fast_experiments.py
@@ -18,34 +18,6 @@ import pydicom
 
 
 with Timer():
-    # MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/nbia/CMMD/D2-0001/07-18-2011-NA-NA-75485/1.000000-NA-12786/"
-    # original = pydicom.dcmread(MAMMOGRAPHY_DATASET_PATH + "1-1.dcm").pixel_array
-    original = cv.imread("image_processing/images/mammography2.png")
-    original = cv.cvtColor(original, cv.COLOR_BGRA2GRAY)
-    modified = original.copy()
-
-    modified = normalize(modified)
-
-    height, width = modified.shape
-
-    kernel_size = (5, 5)
-    modified = cv.GaussianBlur(modified, kernel_size, 0)
-
-    # Convert the new image to binary
-    ret, modified = cv.threshold(modified, 0, 255, cv.THRESH_OTSU)
-
-    # get largest contour
-    contours, hierarchy = cv.findContours(
-        modified, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
-    )
-    big_contour = max(contours, key=cv.contourArea)
-
-    # draw largest contour as white filled on black background as mask
-    mask = np.zeros((height, width), dtype=np.uint8)
-    cv.drawContours(mask, [big_contour], 0, 255, cv.FILLED)
-
-    modified = cv.bitwise_and(original, original, mask=mask)
-
-    modified = high_pass_filter(modified)
-
-    Image.fromarray(modified).show()
+    MAMMOGRAPHY_DATASET_PATH = "/home/matheuscosta/Documents/mammography-dataset/my_subdataset/subdataset_v4/D3-0051/"
+    ds = pydicom.dcmread(MAMMOGRAPHY_DATASET_PATH + "1-1.dcm")
+    ic(ds[0x8, 0x5])
