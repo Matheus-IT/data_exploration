73d9c943f07a1e89187db2d87c8a5cb618f8dffd
matheuscosta <matheuscosta.tech@gmail.com>
2023-12-13 12:38:09 -0300

    Extracting co-ocorrence matrix

--- image_processing/fast_experiments.py
+++ image_processing/fast_experiments.py
@@ -1,10 +1,5 @@
 import matplotlib.pyplot as plt
-from pydicom.pixel_data_handlers.util import apply_voi_lut
 from utils.presentation import (
-    show_pixel_info,
-    show_voi_lut_module,
-    show_file_dataset_info,
-    show_pixel_array_info,
     display_side_by_side,
     show_image,
     show_rgb,
@@ -14,14 +9,45 @@ from utils.presentation import (
     show_ycrcb,
 )
 import cv2 as cv
-from utils.image_normalization import normalize, denormalize
-from utils.filters import opening_filter, closing_filter, high_pass_filter
 from icecream import ic
-import numpy as np
-from utils.elapsed_time import Timer
 from PIL import Image
 import pydicom
+import numpy as np
+import skimage.feature as feature
 
 
-image = cv.imread("image_processing/images/man.jpg")
-show_ycrcb(image)
+img = cv.imread("image_processing/images/man.jpg")
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Param:
+# source image
+# List of pixel pair distance offsets - here 1 in each direction
+# List of pixel pair angles in radians
+glcm = feature.graycomatrix(
+    img, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=256
+)
+
+# Find the GLCM properties
+contrast = feature.graycoprops(glcm, "contrast")
+dissimilarity = feature.graycoprops(glcm, "dissimilarity")
+homogeneity = feature.graycoprops(glcm, "homogeneity")
+energy = feature.graycoprops(glcm, "energy")
+correlation = feature.graycoprops(glcm, "correlation")
+ASM = feature.graycoprops(glcm, "ASM")
+
+print(
+    "contrast:",
+    contrast,
+    "\ndissimilarity:",
+    dissimilarity,
+    "\nhomogeneity:",
+    homogeneity,
+    "\nenergy:",
+    energy,
+    "\ncorrelation:",
+    correlation,
+    "\nASM:",
+    ASM,
+)
+
+# show_image(img)
